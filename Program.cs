2a62dca0b6d9f256923015003119c92e3d62bc24
pacho785 <125918271+pacho785@users.noreply.github.com>
2023-10-04 15:47:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,177 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Bingo humano
+string persona1;
+
+Console.WriteLine("---¿Sabe nadar?---");
+    persona1 = Console.ReadLine(); // Console.ReadLine sirve para poner un valor 
+
+Console.WriteLine(persona1 + " Sabe nadar \n");
+
+//tienen igual color favorito 
+
+
+string persona2;
+
+Console.WriteLine("---Tienen igual color favorito---"); 
+    persona2 = Console.ReadLine();
+
+Console.WriteLine("A " + persona2 + " Le gusta el morado \n ");
+
+//NO gusta futbol
+
+string persona3;
+
+Console.WriteLine("---No le gusta el futbol---"); 
+    persona3 = Console.ReadLine();
+
+Console.WriteLine("A " + persona3 + " no le gusta el futbol \n");
+
+//Le gusta le deporte
+
+string persona4;
+
+Console.WriteLine("---Le gusta el deporte---");
+    persona4 = Console.ReadLine();
+
+Console.WriteLine("A " + persona4 + " le gusta el deporte \n");
+
+//Nacio el mismo mes que usted
+string persona5;
+
+Console.WriteLine("---Nacio en el mismo mes que usted---");
+    persona5 = Console.ReadLine();
+
+Console.WriteLine(persona5 + " Nacio el mismo mes que usted \n");
+
+//Le gustan los buñuelos
+string persona6;
+
+Console.WriteLine("---Le gustan los buñuelos---");
+persona6 = Console.ReadLine();
+
+Console.WriteLine(persona6 + " Le gustan los buñuelos \n");
+
+//Tiene igual pasatiempos
+string persona7;
+
+Console.WriteLine("---Tiene igual pasatiempos---");
+persona7 = Console.ReadLine();
+
+Console.WriteLine(persona7 + " Tiene igual pasatiempos \n");
+
+//Le gustan los niños
+string persona8;
+
+Console.WriteLine("---Le gustan los niños---");
+persona8 = Console.ReadLine();
+
+Console.WriteLine(persona8 + " Le gustan los niños \n");
+
+//Le gustan las matematicas
+string persona9;
+
+Console.WriteLine("---Le gustan las matematicas---");
+persona9 = Console.ReadLine();
+
+Console.WriteLine(persona9 + " Le gustan las matematicas \n");
+
+//Tiene ojos oscuros
+string persona10;
+
+Console.WriteLine("---Tiene ojos oscuros---");
+persona10 = Console.ReadLine();
+
+Console.WriteLine(persona10 + " Tiene ojos oscuros \n");
+
+//le gusta el baile
+string persona11;
+
+Console.WriteLine("---le gusta el baile---");
+persona11 = Console.ReadLine();
+
+Console.WriteLine(persona11 + " le gusta el baile \n");
+
+//es hijo unico
+
+string persona12;
+
+Console.WriteLine("---es hijo unico---");
+persona12 = Console.ReadLine();
+
+Console.WriteLine(persona12 + " es hijo unico \n");
+
+//Le gusta la natilla
+
+string persona13;
+
+Console.WriteLine("---Le gusta la natilla---");
+persona13 = Console.ReadLine();
+
+Console.WriteLine(persona13 + " Le gusta la natilla \n");
+
+//le gusta leer
+
+string persona14;
+
+Console.WriteLine("---Le gustan las matematicas---");
+persona14 = Console.ReadLine();
+
+Console.WriteLine(persona14 + " Le gustan las matematicas \n");
+
+//Tiene familia en otra ciudad
+
+string persona15;
+
+Console.WriteLine("---Tiene familia en otra ciudad---");
+persona15 = Console.ReadLine();
+
+Console.WriteLine(persona15 + " Tiene familia en otra ciudad \n");
+
+//Le recibi un abrazo
+string persona16;
+
+Console.WriteLine("---Le recibi un abrazo---");
+persona16 = Console.ReadLine();
+
+Console.WriteLine(persona16 + " Le recibi un abrazo \n");
+
+//Le gusta investigar
+
+string persona17;
+
+Console.WriteLine("---Le gusta investigar---");
+persona17 = Console.ReadLine();
+
+Console.WriteLine(persona17 + " Le gusta investigar \n");
+
+//No es de su mismo sexo
+
+string persona18;
+
+Console.WriteLine("---No es de su mismo sexo---");
+persona18 = Console.ReadLine();
+
+Console.WriteLine(persona18 + " No es de su mismo sexo \n");
+
+//Se ha ganado algo
+
+string persona19;
+
+Console.WriteLine("---Se ha ganado algo---");
+persona19 = Console.ReadLine();
+
+Console.WriteLine(persona19 + " Se ha ganado algo \n");
+
+//Tener un hijo varon menor de 18 años
+
+string persona20;
+
+Console.WriteLine("---Tener un hijo varon menor de 18 años---");
+persona20 = Console.ReadLine();
+
+Console.WriteLine(persona20 + " Tener un hijo varon menor de 18 años \n");
+
+
+
